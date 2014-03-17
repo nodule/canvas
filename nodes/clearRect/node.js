66482a5103a6dc55ad07d25020d6d281bc6c3add
Rob Halff <rob.halff@gmail.com>
2014-03-17 07:49:23 +0100

    fix args

--- nodes/clearRect/node.js
+++ nodes/clearRect/node.js
@@ -1,9 +1,4 @@
 on.input.context = function() {
-  data.clearRect({
-    x: input.x,
-    y: input.y,
-    width: input.width,
-    height: input.height
-  });
+  data.clearRect(input.x, input.y, input.width, input.height);
   output({ context: data });
 };
